2cbc002251860287e94b49d68520075127df31c5
unknown <glodybulambo@gmail.com>
2021-10-28 23:01:59 +0200

    Material-ui change primary color

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import {Search, ShoppingCartOutlined,Spa} from '@material-ui/icons'
 import { Badge } from '@material-ui/core'
-import {Container,Wrapper,Left,Rigth,Center,Language,SearchContainer,Input,Logo,MenuItem} from './Style'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import {Container,Wrapper,Left,Rigth,Center,Language,
+    SearchContainer,Input,Logo,MenuItem} from './Style'
 
 
 
 const Navbar = () => {
+    const theme = createTheme({
+        palette: {
+            primary: {
+            main: '#6a9113'
+          }
+        }
+      });
+
     return (
+        <MuiThemeProvider theme={theme}>
         <Container>
             <Wrapper>
                 <Left><Logo>KIVUGREEN <Spa style={{color:'#6a9113',fontSize:30}} /></Logo></Left>
@@ -27,6 +38,7 @@ const Navbar = () => {
                 </Rigth>
             </Wrapper>
         </Container>
+        </MuiThemeProvider>
     )
 }
 
