47e5e619a1a6765f5dbe9ad839ef240f53818156
unknown <glodybulambo@gmail.com>
2021-10-31 11:16:45 -0700

    Adding pictures

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,27 +1,41 @@
 
-import mango from "../Images/mango.png"
-import orange from "../Images/orange.jpg"
+import Chemical from "../Images/Chemical.jpg"
+import Potetoes from "../Images/potetoes.jpg"
+import Meats from "../Images/meats.jpg"
+import Fruits from "../Images/fruits.jpg"
+import Vegetables from "../Images/vegetables.jpg"
+import Ananas from "../Images/ananas.png"
+import Apple from "../Images/apple.png"
+import Avocat from "../Images/avocat.png"
+import Banana from "../Images/banana.png"
+import Meat from "../Images/meat.png"
+import Citron from "../Images/citron.png"
+import Brocoli from "../Images/brocoli.png"
+import Cabbage from "../Images/cabbage.png"
+import Potetoe from "../Images/potetoe.png"
+import Paper from "../Images/paper.png"
+import Strawberry from "../Images/strawberry.png"
 
 export const SlideItems=[
     {
         id:1,
-        img:mango,
-        title:"Mango",
-        description: "Footwear and accessories. Shop the best outfits for this season at our online store.",
+        img:Chemical,
+        title:"Organic-Farming",
+        description: "Is our technique of cultivation which avoid the use of chemicals leading to better quality food products with high nutritive values. At the same time, it includes crop rotation, crop residues, organic waste, green manure and use of bio agents for biological pest control",
         bg:"fffafa",
     },
 
     {
         id:2,
-        img:mango,
-        title:"Orange",
-        description: "Footwear and accessories. Shop the best outfits for this season at our online store.",
+        img:Chemical,
+        title:"Bio-Food",
+        description: "We guarantees that at least 95% of the ingredients of agricultural origin come from organic farming.",
         bg:"ffffff",
     },
 
     {
         id:3,
-        img:mango,
+        img:Chemical,
         title:"Banana",
         description: "Footwear and accessories. Shop the best outfits for this season at our online store.",
         bg:"fff5ee",
@@ -31,23 +45,23 @@ export const SlideItems=[
 export const CategoriesItems=[
     {
         id:1,
-        img:orange,
+        img:Fruits,
         title:"Fruits",
     },
 
     {
         id:2,
-        img:orange,
+        img:Vegetables,
         title:"Vegetables",
     },
     {
         id:3,
-        img:orange,
+        img:Potetoes,
         title:"Tubers",
     },
     {
         id:4,
-        img:orange,
+        img:Meats,
         title:"Meat",
     },
 ]
@@ -56,26 +70,50 @@ export const CategoriesItems=[
 export const PopItems=[
     {
         id:1,
-        img:orange,
+        img:Ananas,
     },
     {
         id:2,
-        img:orange,
+        img:Apple,
     },
     {
         id:3,
-        img:orange,
+        img:Avocat,
     },
     {
         id:4,
-        img:orange,
+        img:Banana,
     },
     {
         id:5,
-        img:orange,
+        img:Meat,
     },
     {
         id:6,
-        img:orange,
-    }
+        img:Citron,
+    },
+    {
+        id:7,
+        img:Brocoli,
+    },
+    {
+        id:8,
+        img:Cabbage,
+    },
+    {
+        id:9,
+        img:Potetoe,
+    },
+    {
+        id:10,
+        img:Paper,
+    },
+    {
+        id:11,
+        img:Strawberry,
+    },
+    {
+        id:12,
+        img:Potetoe,
+    },
 ]
