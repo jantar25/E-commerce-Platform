8df982232f69d7ded35d5f6b3398e2ce0b0555b7
unknown <glodybulambo@gmail.com>
2021-11-05 23:29:51 +0200

    Backend start

---
+++ BackEnd/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log("DB Connected"))
+.catch((err)=>console.log(err))
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("BackEnd Server is running on port 5000")
+})
