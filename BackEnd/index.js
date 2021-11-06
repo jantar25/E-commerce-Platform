3b48ae69096673e84c814f919748ce8d55acac1c
unknown <glodybulambo@gmail.com>
2021-11-06 22:25:20 +0200

    Routes done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,11 +3,16 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
+const userRoute = require("./routes/user")
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log("DB Connected"))
 .catch((err)=>console.log(err))
+
+app.use(express.json());
+app.use("/api/user",userRoute)
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log("BackEnd Server is running on port 5000")
