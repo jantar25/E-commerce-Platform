1d64c89e234b0b26977b095a550ed7bdabe0027f
unknown <glodybulambo@gmail.com>
2021-11-20 18:18:11 +0200

    Admin topbar update

--- Admin/src/components/topbar/Topbar.jsx
+++ Admin/src/components/topbar/Topbar.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import "./topbar.css";
+import {Container,Wrapper,Logo,Right,Left,IconContainer,IconBadge,Image} from './style';
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-      <div className="topbarWrapper">
-        <div className="topLeft">
-          <span className="logo">lamaadmin</span>
-        </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
+    <Container>
+      <Wrapper>
+        <Left>
+          <Logo>KIVUGREEN</Logo>
+        </Left>
+        <Right>
+          <IconContainer>
             <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
+            <IconBadge>4</IconBadge>
+          </IconContainer>
+          <IconContainer>
             <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
+            <IconBadge>2</IconBadge>
+          </IconContainer>
+          <IconContainer>
             <Settings />
-          </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
-        </div>
-      </div>
-    </div>
+          </IconContainer>
+          <Image src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="Image"/>
+        </Right>
+      </Wrapper>
+    </Container>
   );
 }
