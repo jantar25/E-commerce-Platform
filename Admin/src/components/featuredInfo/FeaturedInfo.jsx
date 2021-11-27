efcb187a91bce6bfc617f9c542e90b514af3bdb1
unknown <glodybulambo@gmail.com>
2021-11-27 16:37:14 +0200

    admin redux home done

--- Admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ Admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,15 +1,37 @@
 import {Container,Item,Title,MoneyContainer,Money,MoneyRate,Sub} from './style'
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useEffect, useState } from 'react';
+import {userRequest} from '../../requestMethode'
 
 export default function FeaturedInfo() {
+
+  const [income,setIncome] = useState([]);
+  const [percentage,setPercentage] = useState(0);
+
+  useEffect(()=>{
+    const getIncome = async ()=> {
+      try {
+        const res = await userRequest.get("/orders/income");
+        setIncome(res.data)
+        setPercentage((res.data[1].total * 100)/ res.data[0].total - 100);
+      } catch (error) {
+        console.log(error)
+      }
+    }  
+    getIncome();
+  },[]);
+
   return (
     <Container>
       <Item>
         <Title>Revenue</Title>
         <MoneyContainer>
-          <Money>Rwf 5,350,000</Money>
+          <Money>{`Rwf ${income[1].total}`}</Money>
           <MoneyRate>
-            -11.4 <ArrowDownward style={{ color: 'red' }} />
+          % {Math.floor(percentage)}{""} 
+          {percentage<0? <ArrowDownward style={{ color: 'red' }} /> :
+            <ArrowUpward style={{ color: 'green' }} />
+          }
           </MoneyRate>
         </MoneyContainer>
         <Sub>Compared to last month</Sub>
