741055b17ac7b695a8aab3881edb5a6f5e457ffe
unknown <glodybulambo@gmail.com>
2021-12-05 06:13:14 +0200

    product order ongoing

--- FrontEnd/src/Pages/PaySuccess.jsx
+++ FrontEnd/src/Pages/PaySuccess.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { userRequest } from "../requestMethode";
 import { useLocation } from "react-router"
 import styled from "styled-components"
 
@@ -51,13 +54,41 @@ text-align:center;
 const PaySuccess = () => {
     const location = useLocation();
     console.log(location);
+//in Cart.jsx I sent data and cart. Please check that page for the changes.(in video it's only data)
+    const data = location.state.stripeData;
+    const cart = location.state.cart;
+    const currentUser = useSelector((state) => state.user.currentUser);
+    const [orderId, setOrderId] = useState(null);
+
+    useEffect(() => {
+    const createOrder = async () => {
+        try {
+        const res = await userRequest.post("/orders", {
+            userId: currentUser._id,
+            products: cart.products.map((item) => ({
+            productId: item._id,
+            quantity: item._quantity,
+            })),
+            amount: cart.total,
+            address: data.billing_details.address,
+        });
+        console.log(res)
+        setOrderId(res.data._id);
+        } catch {}
+    };
+    data && createOrder();
+    }, [cart, data, currentUser]);
 
     return (
         <Container>
             <Wrap>
                 <Logo>KIVUGREEN</Logo>
                 <TitleContainer><Title>SUCCESSFULL</Title></TitleContainer>
-                <Desc>Your Order is being Prepared.Thanks for choosing KivuGreen Shop</Desc>
+                {orderId
+                ?  <Desc>Your order number ${orderId}` is being Prepared.Thanks for choosing KivuGreen Shop</Desc>
+                :   <Desc>Your order is being Prepared.Thanks for choosing KivuGreen Shop</Desc>}
+                <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+                
             </Wrap>
         </Container>
 
@@ -65,3 +96,8 @@ const PaySuccess = () => {
 }
 
 export default PaySuccess
+
+
+
+
+
