2102542be0a76ab9ae10f2640a5557dca2b723c5
unknown <glodybulambo@gmail.com>
2022-01-23 04:34:11 +0200

    cart done

--- FrontEnd/src/Components/Navbar/Announcement.jsx
+++
@@ -1,9 +0,0 @@
-const Announcement = () => {
-    return (
-        <div className="h-[30px] text-black flex items-center justify-center bg-[#04AA6D]">
-            <p className="text-sm sm:text-md animate-pulse">Free shipping in 3 Months Period for all orders</p>
-        </div>
-    )
-}
-
-export default Announcement
