1e42bf0cb2e8a7a099bf3ad1f7cd5fe2c8bc3554
unknown <glodybulambo@gmail.com>
2022-04-04 03:49:31 +0200

    farmer profile ongoing

--- Admin/src/requestMethode.js
+++ Admin/src/requestMethode.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "https://kivugree.herokuapp.com/api/";
-const currentToken = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser;
+
+const currentToken = localStorage.getItem("persist:root");
 const TOKEN =currentToken? 
-JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken : "";
+JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken : "";
 
 export const publicRequest = axios.create({baseURL:BASE_URL});
 
